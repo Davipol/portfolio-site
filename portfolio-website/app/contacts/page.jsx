ca4a36f4944725e1bde02e59186deb289fd2dd22
Davipol <davi-liz@hotmail.it>
2026-05-18 16:38:31 +0100

    Remove console.log in contact form

--- portfolio-website/app/contacts/page.jsx
+++ portfolio-website/app/contacts/page.jsx
@@ -25,11 +25,7 @@ export default function Contacts() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setStatus("sending");
-    console.log(
-      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
-    );
+
     try {
       await emailjs.send(
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
